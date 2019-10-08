5774fbaf791b68d62c52c21756182d198af030d5
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-08 09:52:49 +0200

    Remove unused import

--- src/main/java/com/github/complate/springdemo/SpringdemoController.java
+++ src/main/java/com/github/complate/springdemo/SpringdemoController.java
@@ -3,8 +3,6 @@ package com.github.complate.springdemo;
 import com.github.complate.ComplateViewResolver;
 import com.github.complate.NashornScriptingBridge;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.View;
@@ -21,13 +19,12 @@ public class SpringdemoController {
     private NashornScriptingBridge engine = new NashornScriptingBridge();
     private ClassPathResource bundle = new ClassPathResource("dist/views.js");
     private Map<String, String> model = new HashMap<>(77);
-
     private ComplateViewResolver resolver = new ComplateViewResolver(engine, bundle);
 
     @GetMapping("/")
     public void index(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        model.put("age", "28");
-        model.put("name", "Ludvig");
+        model.put("age", "99");
+        model.put("name", "John Doe");
         String viewTag = "Person";
         View view = resolver.resolveViewName(viewTag, Locale.US);
         if (view != null) {
